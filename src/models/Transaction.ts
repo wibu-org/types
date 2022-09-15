eab6e976588c539dad99aea85b123cd8cae01d64
andylebo20 <62567315+andylebo20@users.noreply.github.com>
2022-09-15 12:50:59 -0400

    added offer redemption id to transaction

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,6 +1,7 @@
 import { ObjectId } from "mongodb";
 import { Document } from "mongoose";
 import { CardIssuerDocument } from "./CardIssuer";
+import { OfferRedemptionDocument } from "./OfferRedemption";
 import { UserDocument } from "./User";
 
 type Merchant = {
@@ -13,6 +14,7 @@ type Merchant = {
 export interface TransactionDocument extends Document {
   userId: ObjectId | UserDocument;
   cardIssuerId: ObjectId | CardIssuerDocument;
+  offerRedemptionId: ObjectId | OfferRedemptionDocument | null;
   amountInDollars: number;
   mcc: string;
   issuerTransactionId: string;
